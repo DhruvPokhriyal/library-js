17f5d5644a883b074c85b022e54a82ceb51cd3c6
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-11-16 00:37:23 +0530

    Set up more robust and maintainable book management system
    This commit sets up a more robust and maintainable system for book
    management using a loaded library and a general library, it checks if
    the record is already loaded or not in the page, if it is in lib when
    function is called, but not loaded if it will load that in page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const loaded = [];
 
 function book(bookName, author, pages) {
     this.bookName = bookName;
@@ -6,29 +7,34 @@ function book(bookName, author, pages) {
     this.pages = pages;
 }
 
-function addBookToLibrary(bookData) {
-    const page = document.querySelector(".page");
-    const addBook = document.querySelector(".add-book");
-    const record = document.createElement("div");
-    record.classList.add("book");
-    const content = document.createElement("div");
-    content.classList.add("content");
-    record.appendChild(content);
-    const bname = document.createElement("div");
-    bname.classList.add("bname");
-    const bauth = document.createElement("div");
-    bauth.classList.add("bauth");
-    const bpages = document.createElement("div");
-    bpages.classList.add("bpages");
-    content.appendChild(bname);
-    content.appendChild(bauth);
-    content.appendChild(bpages);
-    bname.textContent = bookData.bookName;
-    bauth.textContent = bookData.author;
-    bpages.textContent = bookData.pages;
-    page.insertBefore(record, addBook);
+function addBookToLibrary() {
+    for (bookData of myLibrary) {
+        if (!loaded.includes(bookData)) {
+            const page = document.querySelector(".page");
+            const addBook = document.querySelector(".add-book");
+            const record = document.createElement("div");
+            record.classList.add("book");
+            const content = document.createElement("div");
+            content.classList.add("content");
+            record.appendChild(content);
+            const bname = document.createElement("div");
+            bname.classList.add("bname");
+            const bauth = document.createElement("div");
+            bauth.classList.add("bauth");
+            const bpages = document.createElement("div");
+            bpages.classList.add("bpages");
+            content.appendChild(bname);
+            content.appendChild(bauth);
+            content.appendChild(bpages);
+            bname.textContent = bookData.bookName;
+            bauth.textContent = bookData.author;
+            bpages.textContent = bookData.pages;
+            page.insertBefore(record, addBook);
+            loaded.push(bookData);
+        }
+    }
 }
 
 let book1 = new book("Harry Potter", "J.K. Rowlings", 255);
 myLibrary.push(book1);
-addBookToLibrary(myLibrary[myLibrary.length - 1]);
+addBookToLibrary();
